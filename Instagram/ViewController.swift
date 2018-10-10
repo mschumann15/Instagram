9af6077841cb8e040e929e089eeeda255cbebf2d
Marcus <marcus.eshs@gmail.com>
2018-10-09 19:54:19 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Marcus Schumann on 10/9/18.
+//  Copyright © 2018 Marcus Schumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
