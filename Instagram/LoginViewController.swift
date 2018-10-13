9053c78be1502ddf1c3aa672126763f80df4ed02
Marcus <marcus.eshs@gmail.com>
2018-10-12 20:06:32 -0700

    Adding Repo

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Marcus Schumann on 10/9/18.
+//  Copyright © 2018 Marcus Schumann. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: AnyObject) {
+        
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: AnyObject) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground {
+            (success: Bool, error: Error?) -> Void in
+            if success {
+                print("Yay, Created a User!")
+            } else {
+                print(error?.localizedDescription)
+                if error?._code == 202{
+                    print("Username is taken")
+                }
+            }
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
